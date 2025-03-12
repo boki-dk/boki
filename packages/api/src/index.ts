6458e4061b5f8e5d268e036ad1d0bc05278ceee9
Peter Graugaard <git@graugaard.me>
2025-03-12 14:12:25 +0100

    Add basic nybolig scraping endpoint for test

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { ofetch } from 'ofetch'
 
 const app = new Hono()
 
@@ -16,9 +17,67 @@ app.get('/listings/:listingId', (c) => {
   return c.text(`Listing ${l}`)
 })
 
-app.get('/listings/:listingId/edit', (c) => {
-  const l = c.req.param('listingId')
-  return c.text(`Edit listing ${l}`)
+app.get('/nybolig/listings', async (c) => {
+  const listings = await ofetch('https://www.nybolig.dk/api/search/cases/find', {
+    method: 'POST',
+    body: {
+      siteName: 'nybolig',
+      municipalityCodes: [],
+      municipalityNames: [],
+      cityNames: [],
+      supplementaryCityNames: [],
+      postalCodes: [],
+      postalCodeRanges: [],
+      streetNames: [],
+      streetNumbers: [],
+      propertyCategories: [],
+      businessPropertyCategories: [],
+      isRental: false,
+      rentalProjects: null,
+      saleProjects: null,
+      hasBalcony: null,
+      hasElevator: null,
+      petsAllowed: null,
+      flexProperty: null,
+      isSold: false,
+      isSaleInProgress: false,
+      polygon: [],
+      minPrice: 0,
+      maxPrice: 0,
+      minOwnerExpenses: 0,
+      maxOwnerExpenses: 0,
+      minHouseSubsidy: 0,
+      maxHouseSubsidy: 0,
+      minRent: 0,
+      maxRent: 0,
+      freeText: '',
+      minFloors: 0,
+      maxFloors: 0,
+      minAddressFloor: 0,
+      maxAddressFloor: 0,
+      minRooms: 0,
+      maxRooms: 0,
+      minEnergyClassification: 0,
+      maxEnergyClassification: 0,
+      minBuiltYear: 0,
+      maxBuiltYear: 0,
+      minDaysForSale: 0,
+      maxDaysForSale: 0,
+      minSize: 0,
+      maxSize: 0,
+      minPlotSize: 0,
+      maxPlotSize: 0,
+      minPlotSizeHa: 0,
+      maxPlotSizeHa: 0,
+      minFarmBuildingsSize: 0,
+      maxFarmBuildingsSize: 0,
+      sort: 0,
+      similarCaseId: '',
+      scrollToken: '',
+      top: 10,
+    },
+  })
+  return c.json(listings)
 })
 
 serve(
