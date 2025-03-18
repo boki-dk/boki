a36f6dbcf2a5147c6a2590e7f4eae77fa4230cb4
Peter Graugaard <git@graugaard.me>
2025-03-18 22:25:46 +0100

    Update meta

--- packages/www/app/routes/home.tsx
+++ packages/www/app/routes/home.tsx
@@ -1,13 +1,10 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import { Welcome } from '../welcome/welcome'
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: 'Boki' }, { name: 'description', content: 'Find dit næste hjem med Boki' }]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <Welcome />
 }
