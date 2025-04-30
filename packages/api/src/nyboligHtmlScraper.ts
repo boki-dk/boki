95bfed733c4a3a4353fe46b7bd9d5cd05a96b634
Nicholas Lentfer Tachibana Kristiansen <nicholas@kristiansenz.com>
2025-04-30 15:51:03 +0200

    mroe HTML scraping

--- packages/api/src/nyboligHtmlScraper.ts
+++ packages/api/src/nyboligHtmlScraper.ts
@@ -4,7 +4,7 @@ import { Hono } from 'hono'
 import { ofetch } from 'ofetch'
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { addressesTable, listingsTable, listingTypesTable, scrapedListingsTable } from './db/schema.js'
-import { and, eq, isNull, sql } from 'drizzle-orm'
+import { and, eq, isNull, sql, sum } from 'drizzle-orm'
 import * as schema from './db/schema.js'
 import { HTMLRewriter } from 'htmlrewriter'
 
@@ -22,6 +22,17 @@ export async function scrapeListing(url: string) {
       const src = el.getAttribute('src')
       if (src) {
         images.push(src)
+      }
+    },
+  })
+
+  const floorplanImages: string[] = []
+
+  rewriter.on('#hero-slider-floorplan img', {
+    element: (el) => {
+      const src = el.getAttribute('src')
+      if (src) {
+        floorplanImages.push(src)
       }
     },
   })
@@ -48,7 +59,78 @@ export async function scrapeListing(url: string) {
     },
   })
 
+  let type = ''
+
+  rewriter.on('.case-facts__box-title__type', {
+    text: ({ text }) => {
+      type += text
+    },
+  })
+
+  let price = ''
+
+  rewriter.on('.case-facts__box-title__price', {
+    text: ({ text }) => {
+      price += text
+    },
+  })
+
+  const caseFacts: Record<string, string | null> = {}
+  let caseFactsTitle = ''
+  let caseFactsValue = ''
+
+  rewriter.on('.case-facts__box-inner-wrap span:first-child ', {
+    element: (el) => {
+      caseFactsTitle = ''
+    },
+
+    text: ({ text, lastInTextNode }) => {
+      caseFactsTitle += text
+      // if (lastInTextNode) {
+      //   caseFactsTitle = caseFactsTitle.replace(/&#230;/g, 'æ')
+      //   caseFactsTitle = caseFactsTitle.replace(/&#248;/g, 'ø')
+      //   caseFactsTitle = caseFactsTitle.replace(/&#229;/g, 'å')
+      //   caseFactsTitle = caseFactsTitle.replace(/&#198;/g, 'Æ')
+      //   caseFactsTitle = caseFactsTitle.replace(/&#216;/g, 'Ø')
+      //   caseFactsTitle = caseFactsTitle.replace(/&#197;/g, 'Å')
+      //   caseFactsTitle = caseFactsTitle.replace/&sup2;/g, '²')
+      // }
+    },
+  })
+
+  rewriter.on('.case-facts__box-inner-wrap :nth-child(2) ', {
+    element: (el) => {
+      caseFactsValue = ''
+    },
+
+    text: ({ text, lastInTextNode }) => {
+      caseFactsValue += text
+      if (lastInTextNode) {
+        caseFacts[caseFactsTitle] = caseFactsValue
+      }
+    },
+  })
+
   const _text = await rewriter.transform(response).text()
 
-  return { images, title, description }
+  const areaFloor = caseFacts?.['Boligareal: '] ?? null
+  const rooms =
+    caseFacts?.['Stue/V&#230;relser: ']
+      ?.split('/')
+      .map(Number)
+      .reduce((acc, val) => acc + val, 0) ?? 0
+  
+  const yearBuilt = caseFacts?.['Bygge&#229;r: '] ?? null
+
+  return {
+    //images,
+    title,
+    description,
+    //floorplanImages,
+    type,
+    price: Number(price.replace(/\D/g, '')),
+    caseFacts,
+    areaFloor: Number(areaFloor?.split(' ')[0].replace(/\D/g, '') ?? 0),
+    rooms,
+  }
 }
