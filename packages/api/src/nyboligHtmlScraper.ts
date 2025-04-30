5dabd1822b5f8955544ad6612d9cc48b51039019
Lucas Frandsen <s231316@dtu.dk>
2025-04-30 14:54:06 +0200

    More work on html scraper

--- packages/api/src/nyboligHtmlScraper.ts
+++ packages/api/src/nyboligHtmlScraper.ts
@@ -16,11 +16,39 @@ export async function scrapeListing(url: string) {
   const images: string[] = []
 
   rewriter.on('#hero-slider-photo img', {
-    element: (el: any) => {
+    element: (el) => {
       //   console.log('src', el.getAttribute('src'))
-      images.push(el.getAttribute('src'))
+
+      const src = el.getAttribute('src')
+      if (src) {
+        images.push(src)
+      }
     },
   })
+
+  let title = ''
+
+  rewriter.on('h2.case-facts__title', {
+    text: ({ text }) => {
+      title += text
+    },
+  })
+
+  let description = ''
+
+  rewriter.on('div.wysiwyg.foldable-spot__container p', {
+    text: ({ text }) => {
+      description += text
+    },
+  })
+
+  rewriter.on('div.wysiwyg.foldable-spot__container p br', {
+    element: (el) => {
+      description += '<br>'
+    },
+  })
+
   const _text = await rewriter.transform(response).text()
-  return images
+
+  return { images, title, description }
 }
