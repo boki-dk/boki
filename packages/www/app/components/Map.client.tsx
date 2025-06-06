0f4c3fb0048be891c67b770820d51219bff513a1
Lucas Frandsen <s231316@dtu.dk>
2025-06-06 16:25:39 +0200

    Changed map style

--- packages/www/app/components/Map.client.tsx
+++ packages/www/app/components/Map.client.tsx
@@ -1,16 +1,22 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import type { ExtractSchema } from 'hono/types'
 import type { AppType } from 'api/src/index'
+import { PMTiles, leafletRasterLayer } from 'pmtiles'
 
 type Listing = ExtractSchema<AppType>['/listings']['$get']['output']['listings'][number]
+
 export function Map({ listing }: { listing: Listing }) {
   const location = listing.address.location
+  // const map = useMap()
+  // const p = new PMTiles('https://demo-bucket.protomaps.com/v4.pmtiles')
+  // leafletRasterLayer(p, {}).addTo(map)
+
   return (
     <MapContainer className="w-full h-142 rounded-xl" center={[location.y, location.x]} zoom={15} scrollWheelZoom={false}>
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+        attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; OpenStreetMap contributors'
       />
       <Marker position={[location.y, location.x]}>
         {/* <Popup>
