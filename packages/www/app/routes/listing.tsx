1c7b35b6aff82c2637af090977b91cc7db185d21
Lucas Frandsen <s231316@dtu.dk>
2025-06-06 14:50:09 +0200

    Map working exactly as intended :)

--- packages/www/app/routes/listing.tsx
+++ packages/www/app/routes/listing.tsx
@@ -4,12 +4,13 @@ import type { AppType } from 'api/src/index'
 import type { ExtractSchema } from 'hono/types'
 import { listingsRelations } from 'api/src/db/schema'
 import useEmblaCarousel from 'embla-carousel-react'
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { Icon } from '@iconify/react'
 import { Link } from 'react-router'
 import { Button } from '~/components/ui/button'
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '~/components/ui/card'
 import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures'
-import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
+import { Map } from '~/components/Map.client'
 
 type Listing = ExtractSchema<AppType>['/listings/:listingId']['$get']['output']
 
@@ -44,6 +45,11 @@ export default function Listings({ loaderData }: Route.ComponentProps) {
       emblaApi.scrollTo(floorPlanIndex)
     }
   }, [emblaApi, listing.images])
+
+  const [showMap, setShowMap] = useState(false)
+  useEffect(() => {
+    setShowMap(true)
+  })
 
   return (
     <div className="grid grid-cols-3 gap-4 p-10">
@@ -81,88 +87,79 @@ export default function Listings({ loaderData }: Route.ComponentProps) {
         </div>
         <h1 className="font-bold text-4xl py-2">{listing.address.displayName}</h1>
       </div>
-      <div className="bg-gray-100 p-4 rounded-lg space-y-3">
-        <h2 className="font-bold text-4xl mb-6">Detaljer</h2>
-        <ul className="space-y-3">
-          <li className="flex">
-            <span className="font-extrabold mr-2 shrink-0">Adresse:</span>{' '}
-            <span className="ml-auto flex-1 text-right break-words">{listing.address.displayName}</span>
-          </li>
-          {listing.status !== 'sold' && (
+      <Card>
+        <CardHeader>
+          <CardTitle className="font-extrabold text-4xl">Detaljer</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ul className="space-y-3">
             <li className="flex">
-              <span className="font-extrabold">Pris:</span>
-              <span className="ml-auto">{listing.price} kr.</span>
+              <span className="font-extrabold mr-2 shrink-0">Adresse:</span>{' '}
+              <span className="ml-auto flex-1 text-right break-words">{listing.address.displayName}</span>
             </li>
-          )}
-          {listing.areaFloor !== 0 && (
+            {listing.status !== 'sold' && (
+              <li className="flex">
+                <span className="font-extrabold">Pris:</span>
+                <span className="ml-auto">{listing.price} kr.</span>
+              </li>
+            )}
+            {listing.areaFloor !== 0 && (
+              <li className="flex">
+                <span className="font-extrabold">Størrelse:</span> <span className="ml-auto">{listing.areaFloor} m²</span>
+              </li>
+            )}
+            {listing.areaBasement !== 0 && (
+              <li className="flex">
+                <span className="font-extrabold">Kælderareal:</span> <span className="ml-auto">{listing.areaBasement} m²</span>
+              </li>
+            )}
+            {listing.areaLand !== 0 && (
+              <li className="flex">
+                <span className="font-extrabold">Grundareal:</span> <span className="ml-auto">{listing.areaLand} m²</span>
+              </li>
+            )}
             <li className="flex">
-              <span className="font-extrabold">Størrelse:</span> <span className="ml-auto">{listing.areaFloor} m²</span>
+              <span className="font-extrabold">Værelser:</span> <span className="ml-auto">{listing.rooms}</span>
             </li>
-          )}
-          {listing.areaBasement !== 0 && (
+            {listing.bathroomCount !== null && (
+              <li className="flex">
+                <span className="font-extrabold">Badeværelser:</span> <span className="ml-auto">{listing.bathroomCount}</span>
+              </li>
+            )}
+            {listing.floors !== null && (
+              <li className="flex">
+                <span className="font-extrabold">Etager:</span> <span className="ml-auto">{listing.floors}</span>
+              </li>
+            )}
+            {listing.yearBuilt && (
+              <li className="flex">
+                <span className="font-extrabold"> {listing.yearRenovated ? 'Opført / Renoveret' : 'Opført'} </span>
+                <span className="ml-auto">
+                  {listing.yearRenovated ? `${listing.yearBuilt} / ${listing.yearRenovated}` : listing.yearBuilt}
+                </span>
+              </li>
+            )}
             <li className="flex">
-              <span className="font-extrabold">Kælderareal:</span> <span className="ml-auto">{listing.areaBasement} m²</span>
+              <span className="font-extrabold">Oprettet:</span>
+              <span className="ml-auto">{new Date(listing.createdAt).toLocaleDateString()}</span>
             </li>
-          )}
-          {listing.areaLand !== 0 && (
-            <li className="flex">
-              <span className="font-extrabold">Grundareal:</span> <span className="ml-auto">{listing.areaLand} m²</span>
+            <li className="flex justify-center py-5">
+              <Link
+                className="font-bold text-white text-2xl bg-gradient-to-r from-pink-500 to-red-500  px-4 py-2 rounded-2xl outline-3 outline-blue-500 hover:outline-6 transition-all duration-300"
+                to={listing.sourceUrl}
+              >
+                Gå til mægler
+              </Link>
             </li>
-          )}
-          <li className="flex">
-            <span className="font-extrabold">Værelser:</span> <span className="ml-auto">{listing.rooms}</span>
-          </li>
-          {listing.bathroomCount !== null && (
-            <li className="flex">
-              <span className="font-extrabold">Badeværelser:</span> <span className="ml-auto">{listing.bathroomCount}</span>
-            </li>
-          )}
-          {listing.floors !== null && (
-            <li className="flex">
-              <span className="font-extrabold">Etager:</span> <span className="ml-auto">{listing.floors}</span>
-            </li>
-          )}
-
-          {listing.yearBuilt && (
-            <li className="flex">
-              <span className="font-extrabold"> {listing.yearRenovated ? 'Opført / Renoveret' : 'Opført'} </span>
-              <span className="ml-auto">
-                {listing.yearRenovated ? `${listing.yearBuilt} / ${listing.yearRenovated}` : listing.yearBuilt}
-              </span>
-            </li>
-          )}
-          <li className="flex">
-            <span className="font-extrabold">Oprettet:</span>
-            <span className="ml-auto">{new Date(listing.createdAt).toLocaleDateString()}</span>
-          </li>
-          <li className="flex justify-center py-5">
-            <Link
-              className="font-bold text-white text-2xl bg-gradient-to-r from-pink-500 to-red-500  px-4 py-2 rounded-2xl outline-3 outline-blue-500 hover:outline-6 transition-all duration-300"
-              to={listing.sourceUrl}
-            >
-              Gå til mægler
-            </Link>
-          </li>
-        </ul>
-      </div>
+          </ul>
+        </CardContent>
+      </Card>
       <div className="col-span-3 bg-gray-100 rounded-lg px-2 py-2">
         {/* trust me bro it's fine */}
         {listing.description && <div className="text-gray-700" dangerouslySetInnerHTML={{ __html: listing.description }} />}
         {/* really bro trust me it's fine, don't even worry about it */}
       </div>
-      <div className="colspan-3 bg-gray-100 rounded-lg px-2 py-2">
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
-      </div>
+      <div className="colspan-3 bg-gray-100 rounded-lg px-2 py-2">{showMap && <Map />}</div>
     </div>
   )
 }
