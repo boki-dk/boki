9c80e44665c01b24a1c055401090ced57022f245
Peter Graugaard <git@graugaard.me>
2025-06-23 13:04:39 +0200

    Use correct favicon everywhere

--- packages/www/app/root.tsx
+++ packages/www/app/root.tsx
@@ -27,11 +27,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
-        <link
-          rel="icon"
-          type="image/svg+xml"
-          href="data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cdefs%3E%3ClinearGradient id='myGradient' x1='0%25' y1='0%25' x2='100%25' y2='0%25'%3E%3Cstop offset='0%25' style='stop-color:%23ff23de;' /%3E%3Cstop offset='14.28%25' style='stop-color:%23ff00c3;' /%3E%3Cstop offset='28.56%25' style='stop-color:%23ff00a6;' /%3E%3Cstop offset='42.84%25' style='stop-color:%23ff0088;' /%3E%3Cstop offset='57.12%25' style='stop-color:%23ff006a;' /%3E%3Cstop offset='71.4%25' style='stop-color:%23ff004c;' /%3E%3Cstop offset='85.68%25' style='stop-color:%23ff002e;' /%3E%3Cstop offset='100%25' style='stop-color:%23ff0000;' /%3E%3C/linearGradient%3E%3C/defs%3E%3Cpath fill='url(%23myGradient)' d='M19.31 18.9c.44-.69.69-1.52.69-2.4c0-2.5-2-4.5-4.5-4.5S11 14 11 16.5s2 4.5 4.5 4.5c.87 0 1.69-.25 2.38-.68L21 23.39L22.39 22zm-3.81.1a2.5 2.5 0 0 1 0-5a2.5 2.5 0 0 1 0 5M5 20v-8H2l10-9l10 9h-1.82c-1.18-1.23-2.84-2-4.68-2c-3.58 0-6.5 2.92-6.5 6.5c0 1.29.38 2.5 1.03 3.5z' stroke-width='0.7' stroke='%23fbf7f7'/%3E%3C/svg%3E"
-        />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Boki.dk" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Header />
